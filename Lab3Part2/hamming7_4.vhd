c9d141a13a3e76fe3d3691232575d3884e4e93ba
Xavior Pautin <xpautin@csus.edu>
2024-03-28 23:08:50 -0700

    Minor improvements of spacing, commments, and code of previous labs

--- Lab3Part2/hamming7_4.vhd
+++ Lab3Part2/hamming7_4.vhd
@@ -7,20 +7,17 @@ use work.parity_pack.all;
 
 -- Entity Declaration --
 entity hamming7_4 is
-  port(
-    d7: in std_logic; -- Input bit
-    d6: in std_logic; -- Input bit
-    d5: in std_logic; -- Input bit
-    d3: in std_logic; -- Input bit
+  port (
+    d7, d6, d5, d3: in std_logic; -- Input bits
     dout: out std_logic_vector(7 downto 1) -- Data Out
   );
 end hamming7_4;
 
 -- Architecture Body --
 architecture hamming7_4_arch of hamming7_4 is
-signal p4: std_logic;
-signal p2: std_logic;
-signal p1: std_logic;
+  signal p4: std_logic;
+  signal p2: std_logic;
+  signal p1: std_logic;
 begin
   p4 <= parity(d7 & d6 & d5);
   p2 <= parity(d7 & d6 & d3);
